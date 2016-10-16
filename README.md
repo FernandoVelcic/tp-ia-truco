4a0e6a860b43ee4c5557677a1443baf1b1008dd2
Fernando Velcic <fvelcic@gmail.com>
2016-10-16 12:16:04 -0300

    New predict mode

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 $ virtualenv venv -p /usr/bin/python3
 $ source venv/bin/activate
 $ pip install -r requirements.txt
-$ python main.py --import="training_set.csv"
+$ python main.py --import="training_set.csv" --predict
 ```
 
 
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from training import test_success_rate, train, exportCSV, importCSV
 from sklearn.neural_network import MLPClassifier
 import argparse
+import ast
 
 parser = argparse.ArgumentParser(description="Truco IA")
-parser.add_argument("--export", dest="exportCSV", help="export a training set to csv")
-parser.add_argument("--import", dest="importCSV", help="import a training set from a csv")
+parser.add_argument("--export", dest="exportCSV", help="Export a training set to csv")
+parser.add_argument("--import", dest="importCSV", help="Import a training set from a csv")
+parser.add_argument("--predict", action="store_true", help="Predict mode")
 parser.add_argument("--training_set_size", dest="training_set_size", help="Training set size", type=int, default=100000)
 parser.add_argument("--test_success_rate", dest="test_set_size", help="Test success rate mode", type=int, default=500)
 
@@ -24,6 +26,23 @@ if args.importCSV:
 else:
 	train(clf, args.training_set_size)
 
+if args.predict:
+	print("Predict mode. Write exit to exit")
+	while True:
+		print("Please enter the cards set: ")
+		i = input()
+		
+		if i.strip() == 'exit':
+			break
 
-rate = test_success_rate(clf, args.test_set_size)
-print("Success rate: {0} %".format(round(rate, 2)))
+		cards = ast.literal_eval(i)
+
+		if len(cards) != 40:
+			print("The array is not valid")
+			continue
+
+		result = clf.predict([cards])
+		print("Result: " + str(result));
+else:
+	rate = test_success_rate(clf, args.test_set_size)
+	print("Success rate: {0} %".format(round(rate, 2)))
